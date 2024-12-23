a545704acb734b0f97c96d3ffbe6ca16766be4e6
Hamza Başaran <1basaranhamza@gmail.com>
2024-12-23 15:37:51 +0300

    Created Entities

--- MvcWork/MvcWork/obj/Debug/net6.0/MvcWork.AssemblyInfo.cs
+++ MvcWork/MvcWork/obj/Debug/net6.0/MvcWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7149ebabb745f898bdc567de5b1951052d61b15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5487a28953b087955fc2410c3d1f706921346c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
